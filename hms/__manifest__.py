a190d3bed66a1b6cac1c10b06828f11bfa7c9a9f
HossamZakaria <hosamking2122@gmail.com>
2025-04-27 19:26:35 +0300

    adding user access

--- hms/__manifest__.py
+++ hms/__manifest__.py
@@ -6,11 +6,14 @@
     'author': 'Hossam',
     'depends': ['base'],
     'data': [
+        'security/res_groups.xml',
+        'security/ir.model.access.csv',
+        'security/hms_security.xml',
         'views/patient_views.xml',
         'views/department_views.xml',
+        'views/patient_fix.xml',  # Moved up to define menu_hms_fix
         'views/doctor_views.xml',
         'views/patient_log_views.xml',
-        'views/patient_fix.xml',
         'views/res_partner_views.xml',
     ],
     'installable': True,
